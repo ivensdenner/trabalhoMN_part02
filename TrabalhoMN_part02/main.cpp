fba2b6f46241d49ef4caa1a06c37801c93002f12
Ivens Denner <ivensdenner@gmail.com>
2014-12-01 12:12:09 -0300

    Verificacao de argumentos adicionada.

--- TrabalhoMN_part02/main.cpp
+++ TrabalhoMN_part02/main.cpp
@@ -14,82 +14,90 @@ int main(int argc, char **argv) {
 	std::ifstream in_file;
 	std::ofstream out_file;
 
-	/// Ler arquivo para povoar n, A e b
-	in_file.open(argv[1]);
+	if (argc < 2) {
+		std::cout << "Erro! Arquivo de entrada nao especificado! Para executar esse programa use:" << std::endl;
+		std::cout << "$ ./<nome-do-programa> <nome-do-arquivo-de-entrada>" << std::endl;
+		std::cout << "Saindo..." << std::endl;
+	}
+	else {
 
-	std::cout << "Lendo arquivo " << argv[1] << "..." << std::endl;
-	// Lendo n (dimensão da matriz)
-	in_file >> n;
-	std::cout << "n = " << n << std::endl;
+		/// Ler arquivo para povoar n, A e b
+		in_file.open(argv[1]);
 
-	// Lendo matriz A
-	A1 = new SquareMatrix(n + 1);
-	A2 = new SquareMatrix(n);
-	std::cout << "Matriz A:" << std::endl;
-	for (int i = 0; i < n; i++) {
+		std::cout << "Lendo arquivo " << argv[1] << "..." << std::endl;
+		// Lendo n (dimensão da matriz)
+		in_file >> n;
+		std::cout << "n = " << n << std::endl;
+
+		// Lendo matriz A
+		A1 = new SquareMatrix(n + 1);
+		A2 = new SquareMatrix(n);
+		std::cout << "Matriz A:" << std::endl;
+		for (int i = 0; i < n; i++) {
+			std::cout << "[\t";
+			for (int j = 0; j < n; j++) {
+				double value;
+				in_file >> value;
+				std::cout << value << "\t";
+				A1->set_value(value, i + 1, j + 1);
+				A2->set_value(value, i, j);
+			}
+			std::cout << "]" << std::endl;
+		}
+
+		// Lendo vetor b
+		b1 = new VectorN(n + 1);
+		b2 = new VectorN(n);
+		std::cout << "Vetor b:" << std::endl;
 		std::cout << "[\t";
-		for (int j = 0; j < n; j++) {
+		for (int i = 0; i < n; i++) {
 			double value;
 			in_file >> value;
 			std::cout << value << "\t";
-			A1->set_value(value, i + 1, j + 1);
-			A2->set_value(value, i, j);
+			b1->set_value(value, i + 1);
+			b2->set_value(value, i);
 		}
 		std::cout << "]" << std::endl;
+		in_file.close();
+
+
+		/// Executando fatoracao LU com pivotacao
+		std::cout << "Calculando valores de c pela fatoracao LU com pivotacao..." << std::endl;
+		VectorN *c1 = FatoracaoLU(n, A1, b1);
+
+		/// Executando doolittle sem pivotacao
+		std::cout << "Calculando valores de c pela reducao de doolittle sem pivotacao..." << std::endl;
+		VectorN *c2 = doolittleLU(*A2, *b2);
+
+
+		/// Gravando resultados
+		std::cout << "Gravando resultados no arquivo 'saida.txt'..." << std::endl;
+		out_file.open("saida.txt");
+		out_file << "Valor de c pela fatoracao LU com pivotacao:" << std::endl;
+		std::cout << "Valor de c pela fatoracao LU com pivotacao:" << std::endl;
+		out_file << "c = [\t";
+		std::cout << "c = [\t";
+		for (int i = 0; i < n; i++) {
+			out_file << c1->get_value(i + 1) << "\t";
+			std::cout << c1->get_value(i + 1) << "\t";
+		}
+		out_file << "]" << std::endl;
+		std::cout << "]" << std::endl;
+
+		out_file << "Valor de c pela reducao doolittle sem pivotacao:" << std::endl;
+		std::cout << "Valor de c pela reducao doolittle sem pivotacao:" << std::endl;
+		out_file << "c = [\t";
+		std::cout << "c = [\t";
+		for (int i = 0; i < n; i++) {
+			out_file << c2->get_value(i) << "\t";
+			std::cout << c2->get_value(i) << "\t";
+		}
+		out_file << "]" << std::endl;
+		std::cout << "]" << std::endl;
+		out_file.close();
+
+		std::cout << "Terminado!" << std::endl;
 	}
-
-	// Lendo vetor b
-	b1 = new VectorN(n + 1);
-	b2 = new VectorN(n);
-	std::cout << "Vetor b:" << std::endl;
-	std::cout << "[\t";
-	for (int i = 0; i < n; i++) {
-		double value;
-		in_file >> value;
-		std::cout << value << "\t";
-		b1->set_value(value, i + 1);
-		b2->set_value(value, i);
-	}
-	std::cout << "]" << std::endl;
-	in_file.close();
-
-
-	/// Executando fatoracao LU com pivotacao
-	std::cout << "Calculando valores de c pela fatoracao LU com pivotacao..." << std::endl;
-	VectorN *c1 = FatoracaoLU(n, A1, b1);
-
-	/// Executando doolittle sem pivotacao
-	std::cout << "Calculando valores de c pela reducao de doolittle sem pivotacao..." << std::endl;
-	VectorN *c2 = doolittleLU(*A2, *b2);
-
-
-	/// Gravando resultados
-	std::cout << "Gravando resultados no arquivo 'saida.txt'..." << std::endl;
-	out_file.open("saida.txt");
-	out_file << "Valor de c pela fatoracao LU com pivotacao:" << std::endl;
-	std::cout << "Valor de c pela fatoracao LU com pivotacao:" << std::endl;
-	out_file << "c = [\t";
-	std::cout << "c = [\t";
-	for (int i = 0; i < n; i++) {
-		out_file << c1->get_value(i + 1) << "\t";
-		std::cout << c1->get_value(i + 1) << "\t";
-	}
-	out_file << "]" << std::endl;
-	std::cout << "]" << std::endl;
-
-	out_file << "Valor de c pela reducao doolittle sem pivotacao:" << std::endl;
-	std::cout << "Valor de c pela reducao doolittle sem pivotacao:" << std::endl;
-	out_file << "c = [\t";
-	std::cout << "c = [\t";
-	for (int i = 0; i < n; i++) {
-		out_file << c2->get_value(i) << "\t";
-		std::cout << c2->get_value(i) << "\t";
-	}
-	out_file << "]" << std::endl;
-	std::cout << "]" << std::endl;
-	out_file.close();
-
-	std::cout << "Terminado!" << std::endl;
 
 
 	return 0;
